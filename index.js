6c81807d93fc9e24d9dedc4df76cafcdf5229311
konstantin-sinkov <sinkov.ks@gmail.com>
2022-02-20 00:33:58 +0200

    Lesson_2-hw main routes and pages with .hbs templates

--- index.js
+++ index.js
@@ -1,47 +1,68 @@
+const express = require('express');
+const path = require('path');
+const { engine } = require('express-handlebars');
+const {use} = require("express/lib/router");
+
+const app = express();
+
+app.use(express.json()); //app can get .json files
+app.use(express.urlencoded({extended: true}));
+
+//.hbs engine presets
+app.use(express.static(path.join(__dirname, 'static')));
+app.set('view engine', '.hbs');
+app.engine('.hbs', engine({defaultLayout: false}));
+app.set('views', path.join(__dirname, 'static'));
+
 // 1. /login, поля які треба відрендерити в файлі hbs: firstName, lastName, email(унікальне поле), password,
 // age, city
 // просто зробити темплейт з цим усім і вводити свої дані які будуть пушитися в масив і редірект робити на
 // сторінку з усіма юзерами /users і перевірка чи такий імейл не існує, якщо існує то редірект на еррор пейдж
+let users = [];
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
 // 2. /users просто сторінка з усіма юзерами, але можна по квері параметрам їх фільтрувати по age і city
-// 3. /user/:id сторінка з інфою про одного юзера
-// 4. зробити якщо не відпрацюють ендпоінти то на сторінку notFound редірект
-
-const express = require('express');
-const path = require('path');
-const hbs = require('express-handlebars');
-
-const app = express();
-
-app.use(express.json()); //.use() settings before move on to the endpoint
-app.use(express.urlencoded({extended: true}));
-
-const users = [
-    {
-        name: "Mykola",
-        city: 'Lviv'
-    },
-    {
-        name: "Mykola",
-        city: 'Lviv'
-    },
-    {
-        name: "Mykola",
-        city: 'Lviv'
+app.get('/users', (req, res) => {
+    if (!req.query.age && !req.query.city) {
+        res.render('users', {users: users});
+    } else {
+        // console.log(req.query);
+        let filteredUsers = [];
+        filteredUsers = users.filter(user =>
+            user.age === req.query.age &&
+            user.city === req.query.city
+        );
+        res.render('users', {users: filteredUsers});
     }
-]
+})
 
-app.get('/test', (req, res) => {
-    // res.write('Hello from server! method GET');
-    // res.end(); //'cause server starts process response
-    res.json(users);
-});
+app.post('/login', (req, res) => {
+    if (users.some(user => user.email === req.body.email)) {
+        res.redirect('notFound');
+    } else {
+        users.push({...req.body, id: users.length > 0 ? users.length + 1 : 1});
+        // console.log(users);
+        res.redirect('/users');
+    }
+})
 
-app.post('/send', (req, res) => {
-    // req - WHAT CLIENT SEND TO SERVER, res - WHAT CLIENT GET FROM SERVER
-    console.log(req.body);
-    res.json(`Hello my name is ${req.body.name}`);
+// 3. /user/:id сторінка з інфою про одного юзера
+app.get('/users/:id', (req, res) => {
+    const {id} = req.params; //catch the id in req.params which we type in url
+    // console.log(users);
+    // console.log(req.params);
+    res.render('user', {user: users[+id - 1]});
+})
+
+// 4. зробити якщо не відпрацюють ендпоінти то на сторінку notFound редірект
+app.use((req, res) => {
+  res.render('notFound');
 })
 
 app.listen('5200', () => {
     console.log(('Server 5200 has started'));
 })
+
